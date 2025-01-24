d45544c58ef6c1d88d5749ba070c6ea090a51556
Feelgooddd <fortuneryanc@gmail.com>
2025-01-24 08:00:32 -0500

    Added mail service using an email made just for this project test purpose. Using EmailJS

--- app.js
+++ app.js
@@ -9,42 +9,113 @@ const checkBox = document.getElementById("submit-checkbox");
 
 const consentError = document.getElementById("consent-error");
 const requiredField = document.querySelectorAll(".required-field");
-const errorR = document.querySelectorAll(".error");
+const erroR = document.querySelectorAll(".error");
+const radioDivs = document.querySelectorAll(".query-section__query");
+const radioError = document.getElementById("radio-error");
 
-radioGeneralEnquiry.onclick = function () {
-  if (radioSupportRequest.checked === true) {
-    radioSupportRequest.checked = false;
-  }
-  enquirySection.classList.add("bg-light-green");
-  supportSection.classList.remove("bg-light-green");
+const firstName = document.getElementById("fName");
+const lastName = document.getElementById("lName");
+
+const changeRadioBg = () => {
+  let radioDivArray = [];
+  let i = 0;
+  //run this loop for as many radioDivs exist on the page.
+  radioDivs.forEach((radioDiv) => {
+    radioDivArray.push(radioDiv);
+    //add a function for the radioDiv element and store
+    // it in an array
+    radioDivArray[i].onclick = function () {
+      for (let j = 0; j < 2; j++) {
+        //check THIS radio button aka the button
+        //which is clicked.
+        this.firstElementChild.checked = true;
+
+        if (radioDivArray[j].firstElementChild.checked == true) {
+          this.classList.add("bg-light-green");
+        } else {
+          radioDivArray[j].classList.remove("bg-light-green");
+        }
+      }
+    };
+
+    i++;
+  });
 };
 
-radioSupportRequest.onclick = function () {
-  if (radioGeneralEnquiry.checked === true) {
-    radioGeneralEnquiry.checked = false;
-  }
-  enquirySection.classList.remove("bg-light-green");
-  supportSection.classList.add("bg-light-green");
-};
+//old method for radio buttons.
+// radioGeneralEnquiry.onclick = function () {
+//   if (radioSupportRequest.checked === true) {
+//     radioSupportRequest.checked = false;
+//   }
+//   enquirySection.classList.add("bg-light-green");
+//   supportSection.classList.remove("bg-light-green");
+// };
+
+// radioSupportRequest.onclick = function () {
+//   if (radioGeneralEnquiry.checked === true) {
+//     radioGeneralEnquiry.checked = false;
+//   }
+//   enquirySection.classList.remove("bg-light-green");
+//   supportSection.classList.add("bg-light-green");
+// };
 
 submitButton.addEventListener("click", checkValidity);
 
 function checkValidity() {
-  errorR.forEach(function checker() {
+  let valid = true;
+
+  //Add error message for Input fields.
+  erroR.forEach(function checker() {
     for (i = 0; i <= 3; i++) {
       let x = requiredField[i].value;
-      console.log(x);
-
       if (x == "") {
-        errorR[i].classList.remove("hidden");
+        erroR[i].classList.remove("hidden");
+        valid = false;
       } else {
-        errorR[i].classList.add("hidden");
+        erroR[i].classList.add("hidden");
       }
     }
   }, this);
+
+  //error message for radio buttons
+  if (!radioGeneralEnquiry.checked && !radioSupportRequest.checked) {
+    radioError.classList.remove("hidden");
+    valid = false;
+  } else {
+    radioError.classList.add("hidden");
+  }
+  //error message for checkbox
   if (!checkBox.checked) {
     consentError.classList.remove("hidden");
+    valid = false;
   } else {
     consentError.classList.add("hidden");
   }
+
+  //send email if all is valid.
+  if (valid == true) {
+    sendMail();
+  }
 }
+
+function sendMail() {
+  let subject = "";
+  if (radioGeneralEnquiry.checked == true) {
+    subject = "General Enquiry";
+  } else if (radioSupportRequest.checked == true) {
+    subject = "Support Request";
+  }
+
+  let params = {
+    name: firstName.value + " " + lastName.value,
+    email: document.getElementById("email").value,
+    subject: subject,
+    message: document.getElementById("message-text").value,
+  };
+
+  emailjs
+    .send("service_xy4daip", "template_gzw3he8", params)
+    .then(alert("Email Sent"));
+}
+
+changeRadioBg();
